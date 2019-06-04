52c8d908302a4392fe1ae0ca4f6540ae52e3268d
Drsmile30 <makcuk009@inbox.ru>
2019-06-04 21:12:46 +0500

    start

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static int[] Input()
+        {
+            const int len=5;
+            int[] massive = new int[len];
+            for (int i = 0; i < massive.Length; i++)
+            {
+                Console.WriteLine("Введите " + i + " число: ");
+                massive[i] = int.Parse(Console.ReadLine());
+            }
+            return massive;
+        }
+        static void Sort(int[] massive)
+        {
+            for (int i = 0; i < massive.Length; i++)
+            {
+                for (int j = 0; j < massive.Length; j++)
+                {
+                    if(massive[i]<massive[j])
+                    {
+                        int a = massive[i];
+                        massive[i] = massive[j];
+                        massive[j] = a;
+                        Console.WriteLine(a);
+                    }
+                }
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[]massive = Input();
+            Sort(massive);
+        }
+    }
+}

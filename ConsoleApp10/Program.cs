eb44a43fde6ab7b2b4e175191117bddd635daa01
Drsmile30 <makcuk009@inbox.ru>
2019-06-04 21:26:35 +0500

    Класс people

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -8,38 +8,27 @@ namespace ConsoleApp10
 {
     class Program
     {
-        static int[] Input()
+        class People
         {
-            const int len=5;
-            int[] massive = new int[len];
-            for (int i = 0; i < massive.Length; i++)
-            {
-                Console.WriteLine("Введите " + i + " число: ");
-                massive[i] = int.Parse(Console.ReadLine());
-            }
-            return massive;
+            public string name;
+            public int age;
+            public double size;
         }
-        static void Sort(int[] massive)
-        {
-            for (int i = 0; i < massive.Length; i++)
-            {
-                for (int j = 0; j < massive.Length; j++)
-                {
-                    if(massive[i]<massive[j])
-                    {
-                        int a = massive[i];
-                        massive[i] = massive[j];
-                        massive[j] = a;
-                        Console.WriteLine(a);
-                    }
-                }
-                
-            }
-        }
+
+
         static void Main(string[] args)
         {
-            int[]massive = Input();
-            Sort(massive);
+            People m1 = new People();
+            m1.age = 25;
+            m1.name = "Артём";
+            m1.size = 180.5;
+            People m2 = new People();
+            m2.age = 35;
+            m2.name = "Иван";
+            m2.size = 195.3;
+            Console.WriteLine(m1.age + " " +  m1.name + " "  + m1.size);
+
+                    
         }
     }
 }

73c3ea8e698ea8a1bbc4090b9c809f2061777d7d
Drsmile30 <makcuk009@inbox.ru>
2019-06-04 21:56:29 +0500

    Свойства

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -11,9 +11,9 @@ namespace ConsoleApp10
         class People
         {
             // Поля класса
-            public string name;
-            public int age;
-            public double size;
+            private string name;
+            private int age;
+            private double size;
 
             //Конструктор класса
             public People(string name, int age, double size)
@@ -22,6 +22,19 @@ namespace ConsoleApp10
                 this.age = age;
                 this.size = size;
             }
+            //Свойства
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            //Метод
             public void Show ()
             {
                 Console.WriteLine(name + " " + age + " " + size);
@@ -34,8 +47,8 @@ namespace ConsoleApp10
         {
             People man1 = new People("Вася", 38, 168.3);
             People man2 = new People("Дмитрий", 25, 174);
-            man1.Show();
-            man2.Show();
+            man1.Name = "Ivan";
+            Console.WriteLine(man1.Name);
         }
     }
 }

f789e493a407da7d9b8f546f1d6e12219ebf0f05
Drsmile30 <makcuk009@inbox.ru>
2019-06-06 21:43:49 +0500

    Поля2

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -12,13 +12,16 @@ namespace ConsoleApp10
         {
             // Поля класса
             private string name;
+            private string secondname;
             private int age;
             private double size;
 
+
             //Конструктор класса
-            public People(string name, int age, double size)
+            public People(string name, string secondname, int age, double size)
             {
                 this.name = name;
+                this.secondname = secondname;
                 this.age = age;
                 this.size = size;
             }
@@ -34,21 +37,65 @@ namespace ConsoleApp10
                     name = value;
                 }
             }
-            //Метод
-            public void Show ()
+
+            public string Secondname
             {
-                Console.WriteLine(name + " " + age + " " + size);
+                get
+                {
+                    return secondname;
+                }
+                set
+                {
+                    secondname = value;
+                }
             }
-                
+
+            public int Age
+            {
+                get
+                {
+                    return age;
+                }
+                set
+                {
+                    age = value;
+                }
+            }
+            
+            public double Size
+            {
+                get
+                {
+                    return size;
+                }
+                set
+                {
+                    size = value;
+                }
+            }
+        
+
+
+            //Метод
+            public void Show()
+            {
+                Console.WriteLine(Name + " " + Secondname + " " + Age + " " + Size);
+            }
+
         }
 
 
         static void Main(string[] args)
         {
-            People man1 = new People("Вася", 38, 168.3);
-            People man2 = new People("Дмитрий", 25, 174);
+            People man1 = new People("Вася", "Ivanov", 38, 168.3);
+            People man2 = new People("Дмитрий", "Vasin", 25, 174);
             man1.Name = "Ivan";
-            Console.WriteLine(man1.Name);
+            man1.Secondname = "Petrov";
+            man2.Show();
+            //man2.Age-
+            //man2.age-обращаемся к полю
+
+            
         }
     }
 }
